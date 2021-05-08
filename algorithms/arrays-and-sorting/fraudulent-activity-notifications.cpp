9b86ebfd6ac7d2251f291fcb5565d58907144a71
feliposz <feliposz@gmail.com>
2021-05-08 05:18:18 -0300

    Updating 2021-05-08

---
+++ algorithms/arrays-and-sorting/fraudulent-activity-notifications.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'activityNotifications' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY expenditure
+ *  2. INTEGER d
+ */
+
+int activityNotifications(vector<int> expenditure, int d) {
+    
+    vector<int> recent;
+    
+    int notifications = 0;
+    
+    for (int i = 0; i < d; i++) {
+        recent.push_back(expenditure[i]);
+    }
+    sort(recent.begin(), recent.end());
+
+    // find the median value
+    int hi = d / 2;
+    int lo = hi - ((d % 2) ? 0 : 1);
+            
+    for (int j = d; j < expenditure.size(); j++) {
+        int median2 = recent[lo] + recent[hi];
+
+        // check notification
+        if ((float)expenditure[j] >= median2) {
+            notifications++;
+        }
+        
+        auto it = find(recent.begin(), recent.end(), expenditure[j-d]);
+        recent.erase(it);
+        recent.insert(upper_bound(recent.begin(), recent.end(), expenditure[j]),
+                      expenditure[j]);
+    }
+    
+    return notifications;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int d = stoi(first_multiple_input[1]);
+
+    string expenditure_temp_temp;
+    getline(cin, expenditure_temp_temp);
+
+    vector<string> expenditure_temp = split(rtrim(expenditure_temp_temp));
+
+    vector<int> expenditure(n);
+
+    for (int i = 0; i < n; i++) {
+        int expenditure_item = stoi(expenditure_temp[i]);
+
+        expenditure[i] = expenditure_item;
+    }
+
+    int result = activityNotifications(expenditure, d);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
